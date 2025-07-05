5a26a431e481f3280e0945b8113363329448250b
SpringSecurity <geodb@naver.com>
2025-07-05 12:25:51 +0900

    Init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import DisplayData from './DisplayData'
+function App() {
+  return (
+    <div className="App">
+      <DisplayData />
+{/*}      ----------------------
+      <DisplayData1 />
+      ----------------------
+      <DisplayData2 />
+      ----------------------
+      <DisplayData3 />
+      ----------------------
+      <DisplayData4 /> */}
+    </div>
+  );
+}
+export default App;
